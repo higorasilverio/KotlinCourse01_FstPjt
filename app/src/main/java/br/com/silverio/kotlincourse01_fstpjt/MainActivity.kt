16a86492b06895726453f109ac53a8b5143c83f8
higorasilverio <higorasilverio@outlook.com>
2020-05-18 15:10:29 -0300

    First project finished

--- app/src/main/java/br/com/silverio/kotlincourse01_fstpjt/MainActivity.kt
+++ app/src/main/java/br/com/silverio/kotlincourse01_fstpjt/MainActivity.kt
@@ -2,6 +2,9 @@ package br.com.silverio.kotlincourse01_fstpjt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +12,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun cliqueBotao(view: View){
+
+        var texto = findViewById(R.id.textoExibicao) as TextView
+        texto.setText("Texto alterado!")
+
+    }
+
 }
